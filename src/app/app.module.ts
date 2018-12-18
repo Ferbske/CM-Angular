919f128af01a6406fa9b4186832d30ab3b217681
Unknown <rick.voesenek@hotmail.com>
2018-12-18 14:34:16 +0100

    Signup & Signin created * authGuard + Merge with heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { HttpModule } from "@angular/http";
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {AuthService} from "./components/auth/auth.service";
+import {AuthGuardService} from "./components/auth/auth-guard.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/auth/auth.service.ts
@@ -0,0 +1,65 @@
+import {Injectable} from '@angular/core';
+import {Http} from "@angular/http";
+import {Router} from "@angular/router";
+
+@Injectable()
+
+export class AuthService {
+  url: string = 'https://a2-cm-csfwp-api.herokuapp.com/api';
+
+  constructor(private http: Http, private router: Router) {
+  }
+
+  signupUser(username: string, email: string, password: string) {
+    this.http.post(this.url + '/register',
+      {
+        username: username,
+        email: email,
+        password: password
+      })
+      .subscribe(
+        (response) => {
+          this.router.navigate(['/signin']);
+          return response.status;
+        },
+        (error) => {
+          console.log(error);
+          return error.status;
+        }
+      );
+    return 409
+  }
+
+  signinUser(username: string, password: string) {
+    this.http.post(this.url + '/login',
+      {
+        username: username,
+        password: password
+      })
+      .subscribe(
+        (response) => {
+          this.router.navigate(['/dashboard']);
+          localStorage.setItem("Token", response.json().token);
+          return response.json().response.statusCode;
+        },
+        (error) => {
+          console.log(error);
+          return error.status;
+        }
+      );
+    return 401
+  }
+
+  logout() {
+    localStorage.removeItem("Token");
+    this.router.navigate(['/signin']);
+  }
+
+  getToken() {
+    return localStorage.getItem("Token");
+  }
+
+  isAuthenticated() {
+    return localStorage.getItem("Token") != null;
+  }
+}
