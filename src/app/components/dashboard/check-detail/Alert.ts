2d762156dc2708312478bd85a1621f0c05db238e
Unknown <KNZegelaar@outlook.com>
2019-01-18 00:16:44 +0100

    added alert data

--- src/app/components/dashboard/check-detail/Alert.ts
+++ src/app/components/dashboard/check-detail/Alert.ts
@@ -1,7 +1,15 @@
 export class Alert {
   public id: string;
+  public MerchantID: string;
+  public BuyerName: string;
+  public Amount: string;
+  public Currency: string;
 
-  constructor(id: string) {
+  constructor(id: string, MerchantID: string, BuyerName: string, Amount: string, Currency: string) {
     this.id = id;
+    this.MerchantID = MerchantID;
+    this.BuyerName = BuyerName;
+    this.Amount = Amount;
+    this.Currency = Currency;
   }
 }
